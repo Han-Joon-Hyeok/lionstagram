a15ba66b4c17c20788d54044be76bdef07b521d1
Han-Joon-Hyeok <virq@naver.com>
2020-02-13 16:57:19 +0900

    Modify models.py

--- page/models.py
+++ page/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('publish')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
